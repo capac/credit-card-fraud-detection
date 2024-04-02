d81636cfc53f5982fcbea57404ee23829d3f1b40
capac <capac@users.noreply.github.com>
2024-04-02 18:51:51 +0100

    Converted Jupyter notebook to script

---
+++ test-assignment.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from collections import Counter
+from sklearn.metrics import mutual_info_score
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import balanced_accuracy_score
+from imblearn.pipeline import make_pipeline
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+data_path = Path().cwd() / 'data'
+data_df = pd.read_csv(data_path / 'transactions_obf.csv',
+                      parse_dates=['transactionTime'],
+                      dtype={'availableCash': np.float64,
+                             'transactionAmount': np.float64})
+data_df.sort_values(by='transactionTime', inplace=True)
+data_df[['eventId', 'accountNumber',
+         'merchantId', 'mcc',
+         'merchantCountry', 'merchantZip',
+         'posEntryMode']] = data_df[['eventId', 'accountNumber',
+                                     'merchantId', 'mcc',
+                                     'merchantCountry', 'merchantZip',
+                                     'posEntryMode']].astype('string')
+
+print(data_df.info())
+
+print(data_df.describe())
+
+print('Number of unique entries: ')
+print(f'{data_df.nunique().sort_values(ascending=False)}')
+
+print(f'Number of missing values:\n{data_df.isnull().sum()}')
+
+percent_missing = 100*data_df.isnull().sum()/data_df.shape[0]
+print(f'Percentage of missing values:\n{round(percent_missing, 1)}')
+
+merchant_df = data_df.merchantZip.value_counts(dropna=False, normalize=True)
+print(f'Sorted list of normalized merchantZip codes:\n{merchant_df[:10]}')
+
+print('''Percentage of NAs and 0s in 'merchantZip':''')
+print(f'''{round(100*((data_df.merchantZip == '0').sum() +
+                      data_df.merchantZip.isna().sum())/len(data_df), 2)}%.''')
+
+data_df.drop('merchantZip', axis=1, inplace=True)
+
+data_path = Path().cwd() / 'data'
+labels_df = pd.read_csv(data_path / 'labels_obf.csv',
+                        parse_dates=['reportedTime'])
+labels_df.sort_values(by='reportedTime', inplace=True)
+
+print(labels_df.info())
+
+print(f'Number of unique entries in labels dataset:\n{labels_df.nunique()}')
+
+data_df['fraudCase'] = data_df.eventId.isin(labels_df.eventId).astype(bool)
+
+neg, pos = np.bincount(data_df['fraudCase'])
+total = neg + pos
+print('Total: {}\nPositive: {} ({:.2f}% of total)\n'.format(
+    total, pos, 100 * pos / total))
+
+
+def mutual_info(x):
+    return mutual_info_score(x, data_df.fraudCase)
+
+
+categorical_list = list(data_df.select_dtypes('string'))
+numerical_list = list(data_df.select_dtypes('number'))
+
+print('Mutual information score:')
+mutual_df = data_df[categorical_list].apply(mutual_info)
+print(f'{mutual_df.sort_values(ascending=False)}')
+
+corr_df = data_df[numerical_list].corr()
+print(f'Correlation matrix of numerical features:\n{corr_df}')
+
+data_df.drop(['eventId', 'transactionTime', 'merchantId'],
+             axis=1, inplace=True)
+categorical_list = list(data_df.select_dtypes('string'))
+
+selected_list = categorical_list + numerical_list
+dicts = data_df[selected_list].to_dict(orient='records')
+dv = DictVectorizer(sparse=False)
+dicts_arr = dv.fit_transform(dicts)
+print(f'Shape of processed dataframe: {dicts_arr.shape}')
+
+feature_names_list = dv.get_feature_names_out(selected_list)
+
+processed_df = pd.DataFrame(dicts_arr, columns=feature_names_list)
+
+y = data_df['fraudCase']
+X = processed_df
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                    stratify=y,
+                                                    random_state=42)
+
+print('Ratio of non-fraud and fraud cases:')
+print({key: round(value / len(y), 4) for key, value in Counter(y).items()})
+
+
+processed_categorical_list = \
+    set(feature_names_list) - set(['transactionAmount', 'availableCash'])
+
+model_dict = {'logistic regression': LogisticRegression(random_state=0,
+                                                        max_iter=4000),
+              'decision tree': DecisionTreeClassifier(max_depth=6,
+                                                      random_state=0),
+              'random forest': RandomForestClassifier(n_jobs=-1,
+                                                      random_state=0,
+                                                      max_depth=6,
+                                                      n_estimators=150)}
+
+for name, model in model_dict.items():
+    pipeline_ = make_pipeline(RandomUnderSampler(random_state=0), model)
+    cv_results_from_pipeline_ = cross_validate(pipeline_, X_train, y_train,
+                                               scoring="balanced_accuracy",
+                                               return_train_score=True,
+                                               return_estimator=True,
+                                               n_jobs=-1,
+                                               error_score='raise')
+    print(f"Balanced accuracy mean +/- std. dev. for {name}: "
+          f"{cv_results_from_pipeline_['test_score'].mean():.3f} +/- "
+          f"{cv_results_from_pipeline_['test_score'].std():.3f}")
+    scores = []
+    for fold_id, cv_model in enumerate(cv_results_from_pipeline_["estimator"]):
+        scores.append(balanced_accuracy_score(y_test,
+                                              cv_model.predict(X_test)))
+    print(f"Balanced accuracy mean +/- std. dev. for {name} on test dataset: "
+          f"{np.mean(scores):.3f} +/- {np.std(scores):.3f}"
+          f"\n")
